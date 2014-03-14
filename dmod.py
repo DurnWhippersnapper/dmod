82347535584d059065d3030b01094220b820722f
Allen Welkie <allen.welkie@jhuapl.edu>
2014-03-14 13:17:39 -0400

    Converged psk and dpsk into one function. Currently all phase shift keying is done in one function

--- dmod.py
+++ dmod.py
@@ -1,65 +1,30 @@
 import numpy as np
-
-def check_mapping(mp):
-	return (max(mp) < m) and (min(mp) > 0) and (len(np.unique(mp)) == len(mp))
-
-def mpsk_mod(syms, m, phase_offset = 0, symbol_mapping = 'binary'):
+"""
+Phase Shift Keying
+"""
+def mpsk_mod(syms, m, differential = False, phase_offset = 0, symbol_mapping = 'binary'):
 	"""
 	Modulates the symbols in an m-ary phase shift keying.
 
 	Arguments
 	syms -- an iterator of symbols that are in the range [0,m-1]
 	m -- the number of constellation points
+	differential -- whether or not to do differential encoding
 	phase_offset -- the offset, in radians, of the first symbol
-	symbol_mapping -- The mapping of symbol to constellation place. The default is binary, but a user supplied list of length m can be used to define a custom mapping, where symbol_mapping[i] == j indicates symbol i is mapped to the jth constellation point
+	symbol_mapping -- The mapping of symbol to constellation place. The default is
+	binary, but a user supplied list of length m can be used to define a custom 
+	mapping, where symbol_mapping[i] == j indicates symbol i is mapped to the jth 
+	constellation point
 	
 	Outputs
-	A generator of complex points, one for each input symbol. 
+	A generator of complex points, with the number of points equal to len(syms) if
+	non-differential, or equal to len(syms)+1 if differential
 	"""
 
 
-	if symbol_mapping == 'binary':
-		mapping = np.arange(m)
-	elif check_mapping(symbol_mapping):
-		mapping = symbol_mapping
-	else:
-		raise Exception("Badly formatted symbol mapping")
+	def check_mapping(mp):
+		return (max(mp) < m) and (min(mp) > 0) and (len(np.unique(mp)) == len(mp))
 
-	points = np.exp(1j * 2 * np.pi * np.arange(m) / m + phase_offset)
-
-	for s in syms:
-		if s > m-1 or s < 0:
-			raise Exception("Symbol " + s + " out of range [0,m-1]")
-		yield points[mapping[s]]
-
-	#return np.array([points[mapping[s]] for s in syms])
-
-def qpsk_mod(syms, phase_offset=0, symbol_mapping = 'binary'):
-	"""
-	Equivalent to mpsk_mod with m = 4
-	"""
-	return mpsk_mod(syms, 4, phase_offset, symbol_mapping)
-
-def bpsk_mod(syms, phase_offset=0, symbol_mapping = 'binary'):
-	"""
-	Equivalent to mpsk_mod with m = 2
-	"""
-	return mpsk_mod(syms, 2, phase_offset, symbol_mapping)
-
-
-def dpsk_mod(syms, m, phase_offset = 0, symbol_mapping = 'binary'):
-	"""
-	Modulates the symbols in an m-ary differential phase shift keying.
-
-	Arguments
-	syms -- an iterator of symbols that are in the range [0,m-1]
-	m -- the number of constellation points
-	phase_offset -- the offset, in radians, of the first symbol
-	symbol_mapping -- The mapping of symbol to constellation place. The default is binary, but a user supplied list of length m can be used to define a custom mapping, where symbol_mapping[i] == j indicates symbol i is mapped to the jth phase difference
-	
-	Outputs
-	An numpy array of complex points of length len(syms)+1
-	"""
 	if symbol_mapping == 'binary':
 		mapping = np.arange(m)
 	elif check_mapping(symbol_mapping):
@@ -68,21 +33,45 @@ def dpsk_mod(syms, m, phase_offset = 0, symbol_mapping = 'binary'):
 		raise Exception("Badly formatted symbol mapping")
 
 	rotations_rads = 2 * np.pi * np.arange(m) / m
-	last_point = np.complex(1,0)
-	yield last_point
+	points = np.exp(1j * rotations_rads + phase_offset)
+
+	last_point = np.exp(1j * phase_offset)
+	if differential:
+		yield last_point
 	for s in syms:
 		if s > m-1 or s < 0:
 			raise Exception("Symbol " + s + " out of range [0,m-1]")
-		last_point = last_point * np.exp(1j * rotations_rads[s])
-		yield last_point
+
+		if differential:
+			last_point = last_point * np.exp(1j * rotations_rads[mapping[s]])
+			yield last_point
+		else:
+			yield points[mapping[s]]
+
+def qpsk_mod(syms, differential = False, phase_offset=0, symbol_mapping = 'binary'):
+	"""
+	Equivalent to mpsk_mod with m = 4
+	"""
+	return mpsk_mod(syms, 4, differential, phase_offset, symbol_mapping)
+
+def bpsk_mod(syms, differential = False, phase_offset=0, symbol_mapping = 'binary'):
+	"""
+	Equivalent to mpsk_mod with m = 2
+	"""
+	return mpsk_mod(syms, 2, differential, phase_offset, symbol_mapping)
+
+
+def dpsk_mod(syms, m, phase_offset = 0, symbol_mapping = 'binary'):
+	return mpsk_mod(syms, m, True, phase_offset, symbol_mapping)
 
 def opsk_mod(syms, m, phase_offset = 0, symbol_mapping = 'binary'):
 	raise Exception("Not yet implemented")
 
-def fsk_mod():
-	raise Exception("Not yet implemented")
+"""
+Frequency Shift Keying
+"""
 
-def qam_mod():
+def fsk_mod():
 	raise Exception("Not yet implemented")
 
 def discontinuous_bfsk_modulate(bits, h, samplesperbit):
@@ -100,3 +89,11 @@ def discontinuous_bfsk_modulate(bits, h, samplesperbit):
 	bits = np.repeat(bits,samplesperbit)
 	notbits = [0 if a else 1 for a in bits]
 	return sh * bits + sl * notbits
+
+"""
+Amplitude Shift Keying
+"""
+
+def qam_mod():
+	raise Exception("Not yet implemented")
+
