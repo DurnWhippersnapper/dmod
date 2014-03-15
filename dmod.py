358df6169e01577f40577c68d059b937ef25be1f
Allen Welkie <awelkie@desktop.home>
2014-03-15 15:13:00 -0400

    Implemented the fsk_mod function

--- dmod.py
+++ dmod.py
@@ -1,4 +1,8 @@
 import numpy as np
+
+def check_mapping(mp):
+	return (max(mp) < m) and (min(mp) > 0) and (len(np.unique(mp)) == m)
+
 """
 Phase Shift Keying
 """
@@ -20,10 +24,6 @@ def mpsk_mod(syms, m, differential = False, phase_offset = 0, symbol_mapping = '
 	A generator of complex points, with the number of points equal to len(syms) if
 	non-differential, or equal to len(syms)+1 if differential
 	"""
-
-
-	def check_mapping(mp):
-		return (max(mp) < m) and (min(mp) > 0) and (len(np.unique(mp)) == m)
 
 	if symbol_mapping == 'binary':
 		mapping = np.arange(m)
@@ -71,24 +71,58 @@ def opsk_mod(syms, m, phase_offset = 0, symbol_mapping = 'binary'):
 Frequency Shift Keying
 """
 
-def fsk_mod():
-	raise NotImplementedError
+def fsk_mod(syms, m, h, samplespersymbol, continuous_phase = True, symbol_mapping = 'binary'):
+	"""
+	Modulates the symbols in an m-ary frequency shift keying.
 
-def discontinuous_bfsk_modulate(bits, h, samplesperbit):
+	Arguments
+	syms -- an iterator of symbols that are in the range [0,m-1]
+	m -- the number of separate frequencies
+	h -- modulation index, defined as the frequency difference in successive
+	tones over the carrier frequency
+	samplespersymbol -- How many output samples per input symbol
+	continuous_phase -- Whether or not to do continuous phase modulation
+	symbol_mapping -- The mapping of symbol to tone. The default is binary,
+	but a user supplied list of length m can be used to define a custom mapping,
+	where symbol_mapping[i] == j indicates symbol i is mapped to the jth tone.
+
+	The tones are centered around the carrier, with the carrier being at 0 Hz 
+	baseband (feel free to mix the output to move the carrier). Tones are numbered
+	from least frequency (most negative) to greatest frequency. 
+	
+	Outputs
+	A generator of complex points, with the number of points equal to len(syms)*samplespersymbol
+	"""
+
+	if symbol_mapping == 'binary':
+		mapping = np.arange(m)
+	elif check_mapping(symbol_mapping):
+		mapping = symbol_mapping
+	else:
+		raise ValueError("Badly formatted symbol mapping")
+
 	#these are in fractions of fs, not in Hz.
 	#it's a bit weird, but it allows us to be agnostic to
 	#the actual sampling frequency
-	fh = (h / 2.0) / samplesperbit
-	fl = -(h / 2.0) / samplesperbit
+	freqs = np.arange(0, m) * h * 1.0 / samplespersymbol
+	freqs = freqs - (freqs[-1] - freqs[0])/2.0
 
-	t = np.arange(len(bits) * samplesperbit)
-	sl = np.exp(1j * 2 * np.pi * fl * t)
-	sh = np.exp(1j * 2 * np.pi * fh * t)
+	#we keep around the sample number for every sample so we can emulate a switched
+	#oscillator FSK transmitter
+	t = 0
+	#The current phase is simply added to for continuous phase FSK
+	phase = 0
+	for s in syms:
+		if s > m-1 or s < 0:
+			raise IndexError("Symbol " + str(s) + " out of range [0,m-1]")
 
-	bits = [1 if a > 0 else 0 for a in bits]
-	bits = np.repeat(bits,samplesperbit)
-	notbits = [0 if a else 1 for a in bits]
-	return sh * bits + sl * notbits
+		for i in range(samplespersymbol):
+			if continuous_phase:
+				phase = phase + 2 * np.pi * freqs[mapping[s]]
+				yield np.exp(1j * phase)
+			else:
+				yield np.exp(1j * 2 * np.pi * freqs[mapping[s]] * t)
+				t = t + 1
 
 """
 Amplitude Shift Keying
--- dmod_tests.py
+++ dmod_tests.py
@@ -11,19 +11,23 @@ class TestPSK(unittest.TestCase):
 		truth = np.array([1+0j, 0 + 1j, -1 + 0j, 0 - 1j])
 		error = np.sum(np.square(truth - np.array(list(modulated))))
 		self.assertTrue(error < self.epsilon)
+
 	def test_bpsk(self):
 		modulated = dmod.qpsk_mod([0,1,0,1])
 		truth = np.array([1,-1,1,-1])
 		error = np.sum(np.square(truth - np.array(list(modulated))))
 		self.assertTrue(error < self.epsilon)
+
 	def test_differential(self):
 		modulated = dmod.dpsk_mod([0,1,1,0], 4)
 		truth = np.array([1, 1, 1j, -1, -1])
 		error = np.sum(np.square(truth - np.array(list(modulated))))
 		self.assertTrue(error < self.epsilon)
+
 	def test_bad_symbols(self):
 		with self.assertRaises(IndexError):
 			list(dmod.qpsk_mod([0,1,2,3,4]))
+
 	def test_bad_mapping(self):
 		with self.assertRaises(ValueError):
 			list(dmod.mpsk_mod([0,1,2,3], 4, True, 0, [0,1,1,2]))
